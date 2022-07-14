3784410245fdb469bd88138e35a0f77b702c2922
ashish.pandey <ashish.pandey@synoriq.in>
2022-07-14 16:51:22 +0530

    hypertrack-install

--- android/src/main/java/com/hypertrack/sdk/capacitor/HyperTrackSdkPlugin.java
+++ android/src/main/java/com/hypertrack/sdk/capacitor/HyperTrackSdkPlugin.java
@@ -5,17 +5,17 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-
+import com.hypertrack.sdk.HyperTrack;
 @CapacitorPlugin(name = "HyperTrackSdk")
 public class HyperTrackSdkPlugin extends Plugin {
 
     private HyperTrackSdk implementation = new HyperTrackSdk();
-
+    private HyperTrack hyperTrack;
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
-
         JSObject ret = new JSObject();
+        HyperTrack.enableDebugLogging();
         ret.put("value", implementation.echo(value));
         call.resolve(ret);
     }
