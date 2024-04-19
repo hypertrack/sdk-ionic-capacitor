65d34ae29e6e46c5f2d230591cce99541cda1caa
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2024-04-19 17:54:42 +0300

    Fix imports

--- android/src/main/java/com/hypertrack/sdk/capacitor/common/HyperTrackSdkWrapper.kt
+++ android/src/main/java/com/hypertrack/sdk/capacitor/common/HyperTrackSdkWrapper.kt
@@ -3,23 +3,23 @@ package com.hypertrack.sdk.capacitor.common
 import com.hypertrack.sdk.android.HyperTrack
 import com.hypertrack.sdk.android.Json
 import com.hypertrack.sdk.android.Result
-import com.reactnativehypertracksdk.common.Serialization.deserializeDynamicPublishableKey
-import com.reactnativehypertracksdk.common.Serialization.deserializeGeotagData
-import com.reactnativehypertracksdk.common.Serialization.deserializeIsAvailable
-import com.reactnativehypertracksdk.common.Serialization.deserializeIsTracking
-import com.reactnativehypertracksdk.common.Serialization.deserializeMetadata
-import com.reactnativehypertracksdk.common.Serialization.deserializeName
-import com.reactnativehypertracksdk.common.Serialization.serializeDeviceId
-import com.reactnativehypertracksdk.common.Serialization.serializeDynamicPublishableKey
-import com.reactnativehypertracksdk.common.Serialization.serializeErrors
-import com.reactnativehypertracksdk.common.Serialization.serializeIsAvailable
-import com.reactnativehypertracksdk.common.Serialization.serializeIsTracking
-import com.reactnativehypertracksdk.common.Serialization.serializeLocationErrorFailure
-import com.reactnativehypertracksdk.common.Serialization.serializeLocationResult
-import com.reactnativehypertracksdk.common.Serialization.serializeLocationSuccess
-import com.reactnativehypertracksdk.common.Serialization.serializeLocationWithDeviationSuccess
-import com.reactnativehypertracksdk.common.Serialization.serializeMetadata
-import com.reactnativehypertracksdk.common.Serialization.serializeName
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeDynamicPublishableKey
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeGeotagData
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeIsAvailable
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeIsTracking
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeMetadata
+import com.hypertrack.sdk.capacitor.common.Serialization.deserializeName
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeDeviceId
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeDynamicPublishableKey
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeErrors
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeIsAvailable
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeIsTracking
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeLocationErrorFailure
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeLocationResult
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeLocationSuccess
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeLocationWithDeviationSuccess
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeMetadata
+import com.hypertrack.sdk.capacitor.common.Serialization.serializeName
 
 typealias Serialized = Map<String, Any?>
 
