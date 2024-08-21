17d86a24cb326a0b2bd15323fab83080ffcefe0f
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2024-08-22 00:09:16 +0300

    Fix index.js

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import HyperTrack from './HyperTrack/HyperTrack';
+import HyperTrack from './HyperTrack';
 export default HyperTrack;
 export { HyperTrack };
-export { HyperTrackError } from './HyperTrack/data_types/HyperTrackError';
+
+export { HyperTrackError } from './data_types/HyperTrackError';
 export type {
   NotRunning,
   Starting,
-} from './HyperTrack/data_types/internal/LocationErrorInternal';
+} from './data_types/internal/LocationErrorInternal';
 export type {
   LocationError,
   Errors,
-} from './HyperTrack/data_types/LocationError';
-export type { LocationWithDeviation } from './HyperTrack/data_types/LocationWithDeviation';
-export type { Location } from './HyperTrack/data_types/Location';
-export type { Order } from './HyperTrack/data_types/Order';
-export type { OrderStatus } from './HyperTrack/data_types/OrderStatus';
-export type { Result, Success, Failure } from './HyperTrack/data_types/Result';
+} from './data_types/LocationError';
+export type { LocationWithDeviation } from './data_types/LocationWithDeviation';
+export type { Location } from './data_types/Location';
+export type { Order } from './data_types/Order';
+export type { Subscription } from './Subscription';
+export type { OrderStatus } from './data_types/OrderStatus';
+export type { Result, Success, Failure } from './data_types/Result';
